35c07dde23809c98ec7a26587060f41c578333f3
Adrián Molina Elvira <adrimo100@gmail.com>
2022-01-24 20:21:22 +0000

    Globos con contadores por colores

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ let activeBalloons = ballonsMap.length;
 
 let ballonsCounter = {};
 
+for(let color of ballonsMap)  
+    if(ballonsCounter[color] == undefined)
+        ballonsCounter[color] = 1;   
+    else
+        ballonsCounter[color]++;
+
 
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position, color) => {
     // set the color to null on the balloon position
     ballonsMap[position] = null;
     activeBalloons--;
+    ballonsCounter[color]--;
+    document.querySelector("#colours").innerHTML = "";
     render();
 }
 
@@ -24,14 +32,33 @@ const render = () => {
         //return `<div class="balloon active"></div>`; // <--- render each balloon
         if(color == null)
             return `<div class="balloon popped"></div>`;
-        else
-            return `<div class="balloon active" style="background: ${color}" onclick ="popBalloon(${position, color})"></div>`;
+        else{
+            
+            
+            return `<div class="balloon active" style="background: ${color}" onclick ="popBalloon(${position},'${color}')"></div>`;
+        }
     });
 
     document.querySelector("#balloon-count").innerHTML = activeBalloons; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
+    
+
+    //Imprime cuantos globos quedan de cada color
+
+    for(let color in ballonsCounter){
+        document.querySelector("#colours").innerHTML += `<br> <span> ${color.toUpperCase()} balloons left: ${ballonsCounter[color]}</span>`;
+    }
+    
 
     if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+}
+
+console.log(ballonsCounter);
+
+
+let countColours = () => {
+    
+    
 }
 
 // this makes the "render" function trigger when the website starts existing
