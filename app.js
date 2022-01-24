81bac08524e204a01ecb53fe494363fc1437d1c8
Adrián Molina Elvira <adrimo100@gmail.com>
2022-01-24 19:36:36 +0000

    Versión correcta

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let ballonsCounter = {};
 
 
 // poping a balloon is basically turning his color to null (no color)
-const popBalloon = (position, color) => {
+const popBalloon = (position) => {
     // set the color to null on the balloon position
     ballonsMap[position] = null;
     activeBalloons--;
@@ -25,7 +25,7 @@ const render = () => {
         if(color == null)
             return `<div class="balloon popped"></div>`;
         else
-            return `<div class="balloon active" style="background: ${color}" onclick ="popBalloon(${position, color})"></div>`;
+            return `<div class="balloon active" style="background: ${color}" onclick ="popBalloon(${position})"></div>`;
     });
 
     document.querySelector("#balloon-count").innerHTML = activeBalloons; // <-- render the balloon count into the DOM
